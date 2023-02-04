da70a9744ba72d3e02159861bc72875219dc1c15
personal <darkfabiotm@gmail.com>
2023-02-04 16:24:34 -0300

    :sparkles: feat[main]implemented functions

---
+++ PalavraChave/PalavraChave/PalavraChaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalavraChaveApp.swift
+//  PalavraChave
+//
+//  Created by Fábio Carlos de Souza on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PalavraChaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
